cab5ed187e78db9a515ddc2339285f92b14f9404
1301mohit <mohitkr1301@gmail.com>
2019-12-03 17:57:37 +0530

    Add function to report the state census data in a json file as per state alphabetical order

--- src/main/java/com/statesCensusAnalyser/StateCensusAnalyser.java
+++ src/main/java/com/statesCensusAnalyser/StateCensusAnalyser.java
@@ -1,43 +1,52 @@
 package com.statesCensusAnalyser;
 
+import com.google.gson.Gson;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class StateCensusAnalyser {
     private static final String STATE_CENSUS_DATA = "StateCensusData1.csv";
 
+    List<StateCensus> stateCensusLists = new ArrayList();
+
     private int count = 0;
 
-    public String readCsv(String stateCensusData, Class className) throws StateCensusAnalyserException {
-        Iterator iterator = this.builder(stateCensusData, className);
-        while(iterator.hasNext()) {
-            iterator.next();
-            count++;
+    public static String readCsv(String stateCensusData, Class className) throws StateCensusAnalyserException {
+        List stateCensusLists = builder(stateCensusData, className);
+        stateCensusLists.sort(Comparator.comparing(StateCensus::getState));
+        Gson gson = new Gson();
+        String json = gson.toJson(stateCensusLists);
+        try {
+            FileWriter writer = new FileWriter("/home/mohit/Indian-States-Census-Analyser-Problem/src/main/resources/StateCensusData.json");
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if(count == 29)
-            return  "HAPPY";
+
+        if (stateCensusLists.size() == 29)
+            return "HAPPY";
         else
             return "SAD";
     }
 
-    private <T> Iterator<T> builder(String stateCensusData, T className) throws StateCensusAnalyserException {
+    private static <T> List<T> builder(String stateCensusData, T className) throws StateCensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(stateCensusData));
             CsvToBean csvToBean = new CsvToBeanBuilder(reader)
-                    .withType((Class)className)
+                    .withType((Class) className)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
-            Iterator iterator = csvToBean.iterator();
-            return iterator;
-        }
-        catch (IOException e) {
+            List<T> stateCensusList = csvToBean.parse();
+            return stateCensusList;
+        } catch (IOException e) {
             throw new StateCensusAnalyserException("SAD", StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE);
         }
     }
